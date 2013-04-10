c344890f01e2f55989bb6b86e7e7d2b5d3e57ab4
myun2 <myun2@nwhite.info>
2013-04-10 14:01:48 +0900

    new rails project [gudaru].

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gudaru::Application.config.secret_token = '9384efb3ec21810dba8d52401ae447651bdeaf02b27a6aea6e0e4eca0467e49c040d38da69f20d649dc3f08e9849e78b01eaea6eb14fdc9762dc8a6f085ca658'
