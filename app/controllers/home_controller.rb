bb1bef2b9c288de432b1cb2209eaeaa7868d0bc3
myun2 <myun2@nwhite.info>
2013-04-11 13:21:05 +0900

    home#index View

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @messages = Message.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @messages }
+    end
   end
 end
