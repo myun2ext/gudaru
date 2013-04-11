30c6d6b51b74b6fff45b9ffb7da7a4fa7c39d045
myun2 <myun2@nwhite.info>
2013-04-11 13:36:23 +0900

    migrate Channel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411034411) do
+ActiveRecord::Schema.define(:version => 20130411041010) do
+
+  create_table "channels", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "messages", :force => true do |t|
     t.string   "body"
