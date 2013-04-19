cc8c6ac981c9b0c80ad64a874f2f9dadf943b26a
myun2 <myun2@nwhite.info>
2013-04-20 01:31:31 +0900

    Message Model Validation: provisional limit length 256 to body

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :channel
   attr_accessible :body
+
+  # Validations
+  validates :user,    :presence => true
+  validates :channel, :presence => true
+
+  validates :body, :presence => true, :length => { :maximum => 256 }
 end
