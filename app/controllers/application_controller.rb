8ad1890f79816f2c09c8ec0e15d6c87b7571f604
myun2 <myun2@nwhite.info>
2013-04-20 16:36:17 +0900

    locale ja Add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
   include Twitter::Login::Helpers
+
   before_filter :twitter_login, if: :logged_in?
+  before_filter :set_locale  
+
+  #==========
 
   def logged_in?
     !!session[:twitter_user]
+  end
+
+  def logged_in
+    session[:twitter_user]
   end
 
   helper_method :logged_in?
@@ -14,8 +23,15 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
+  #==========
+
   private
   def twitter_login
     @twitter_user = twitter_user
   end
+
+  def set_locale  
+    I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].split(",").first
+    puts "locale=#{I18n.locale}"
+  end  
 end
