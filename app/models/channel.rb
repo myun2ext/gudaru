5785aaf1b6156e34057f28e5987b12b288806dca
myun2 <myun2@nwhite.info>
2013-04-20 19:36:51 +0900

    channel#form Add short_name channel::short_name format, length validation

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,5 +5,8 @@ class Channel < ActiveRecord::Base
   attr_accessible :short_name, :name
 
   validates :owner, :presence => true
-  validates :short_name, :uniqueness => true
+  validates :short_name,
+    :uniqueness => true,
+    :format => { :with => /^[a-z0-9\-]+$/ },
+    :length => { :minimum => 6, :maximum => 24 }
 end
