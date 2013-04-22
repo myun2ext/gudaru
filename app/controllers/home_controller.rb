01d829917495bd049e68f70a4f15d29bef353fac
myun2 <myun2@nwhite.info>
2013-04-23 02:32:22 +0900

    Redirect to new User is user/new

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   include TwitterProfileImageAction
 
   def logged_in
-    redirect_to '/'
+    if tw_first?
+      redirect_to new_user_url
+    else
+      redirect_to root_url
+    end
   end
 end
